1f9948b916b00ebf8e371555aebc6971555d3cc9
Antonio Jr <acdcjunior@gmail.com>
2017-06-16 23:16:09 -0300

    fix(ts): fix lint errors and update some rules

--- .docs/ANOTACOES.md
+++ .docs/ANOTACOES.md
@@ -1,7 +1,13 @@
 # Anotações em geral
 
+### TSLINT
+
+`Antonio`> Pessoas, fiz alguns ajustes no linter de TS. Ajustei algumas regras pra o meu gosto. Caso não gostem da configuração de alguma regra, fiquem aa vontade para mudar (ou conversar em caso de conflitos)!
+
 ### Como gerar o token para deploy do firebase no CI?
 
 ```shell
 firebase login:ci
 ```
+
+Nao precisam se preocupar com isso, coloquei aqui só pra lembrar como fazer. Estamos este token no circle-ci, para fazer o deploy a cada commit.
--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -4,12 +4,12 @@ import { AngularFireAuthModule } from 'angularfire2/auth';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 
 const firebaseConfig = {
-	apiKey: "AIzaSyC5Y8D-BlICHOwdPGJIR6bxXdmdMFC93uM",
-	authDomain: "suasvacinas.firebaseapp.com",
-	databaseURL: "https://suasvacinas.firebaseio.com",
-	projectId: "suasvacinas",
-	storageBucket: "suasvacinas.appspot.com",
-	messagingSenderId: "57819698131"
+  apiKey: "AIzaSyC5Y8D-BlICHOwdPGJIR6bxXdmdMFC93uM",
+  authDomain: "suasvacinas.firebaseapp.com",
+  databaseURL: "https://suasvacinas.firebaseio.com",
+  projectId: "suasvacinas",
+  storageBucket: "suasvacinas.appspot.com",
+  messagingSenderId: "57819698131"
 };
 
 @NgModule({
